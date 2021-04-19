1ad64c8a8f99ac87479d99b0b630ffe0be0af18c
oceanide <oceanide@gmail.com>
2021-04-20 00:16:03 +0900

    Add constructor to MortgageReport

--- java/MortgageCalculator/src/com/oceanide/Main.java
+++ java/MortgageCalculator/src/com/oceanide/Main.java
@@ -9,7 +9,9 @@ public class Main {
         float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
-        var report = new MortgageReport();
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
+        var report = new MortgageReport(calculator);
+
         report.printMortgage(principal, annualInterest, years);
         report.printPaymentSchedule(principal, annualInterest, years);
     }
--- java/MortgageCalculator/src/com/oceanide/MortgageReport.java
+++ java/MortgageCalculator/src/com/oceanide/MortgageReport.java
@@ -3,9 +3,13 @@ package com.oceanide;
 import java.text.NumberFormat;
 
 public class MortgageReport {
-     void printMortgage(int principal, float annualInterest, byte years) {
-        var calculator = new MortgageCalculator(principal, annualInterest, years);
+    private final MortgageCalculator calculator;
 
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    void printMortgage(int principal, float annualInterest, byte years) {
         double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
@@ -19,8 +23,6 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            var calculator = new MortgageCalculator(principal, annualInterest, years);
-
             double balance = calculator.calculateBalance(month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
