1b5d1386e974df28e7df89cd06bbc6fc835279d8
oceanide <oceanide@gmail.com>
2021-04-20 22:31:41 +0900

    Move static member from Main to MortgageCalculator

--- java/MortgageCalculator/src/com/oceanide/MortgageCalculator.java
+++ java/MortgageCalculator/src/com/oceanide/MortgageCalculator.java
@@ -1,6 +1,9 @@
 package com.oceanide;
 
 public class MortgageCalculator {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
     private final int principal;
     private final float annualInterest;
     private final byte years;
@@ -12,8 +15,8 @@ public class MortgageCalculator {
     }
 
     public double calculateBalance(short numberOfPaymentsMade) {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -23,8 +26,8 @@ public class MortgageCalculator {
     }
 
     public double calculateMortgage() {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
